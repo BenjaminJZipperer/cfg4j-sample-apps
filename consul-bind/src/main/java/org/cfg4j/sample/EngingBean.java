4ca8516e374ba1bc6979cc055301bab6784ccf93
Benjamin Zipperer <benjamin-zipperer@web.de>
2020-10-24 13:41:26 +0200

    final commit

--- consul-bind/src/main/java/org/cfg4j/sample/EngingBean.java
+++ consul-bind/src/main/java/org/cfg4j/sample/EngingBean.java
@@ -1,5 +1,5 @@
 package org.cfg4j.sample;
-
+/* test git stash example: most bottom method was stashed then  method above was created then final commit */
 public class EngineBean
 {
 	private String ENGINE_STATUS = "on_listening";
@@ -11,17 +11,16 @@ public class EngineBean
 	{
 		return this.result;
 	}
-<<<<<<< Updated upstream
 
-	public int i_am_new_whil_benjamin_worked_on_station_on_something_else()
-{
-	return -1000800; // limit in my scope?
-}
-=======
+
+	public int i_am_new_while_benjamin_worked_on_station_on_something_else()
+	{
+		return -1000800; // limit in my scope?
+	}
 	//first request User-Story
 	public void setResultA(String engine_status)
 	{
 		this.ENGINE_STATUS = engine_status;
 	}
->>>>>>> Stashed changes
+
 }
